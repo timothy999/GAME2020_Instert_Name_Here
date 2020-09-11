ad705d4d32d33c4f6943c6e669f002958d2ca17f
timothy <timklaassen@hotmail.nl>
2020-09-11 16:17:28 +0200

    Sort of Working Version

--- Workshops/2020-09-11, Workshop 1/Assets/Scripts/PlayerController.cs
+++ Workshops/2020-09-11, Workshop 1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public Vector3 respawnPosition;
+    public float minimumYPos = -5f;
+
+    private Rigidbody rb;
+    private Transform playerTransform;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerTransform = GetComponent<Transform>();
+
+        if (respawnPosition == null)
+        {
+            respawnPosition = playerTransform.position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (playerTransform.position.y < minimumYPos)
+        {
+            RespawnPlayer();
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void RespawnPlayer()
+    {
+        playerTransform.position = respawnPosition;
+        rb.velocity = Vector3.zero;
     }
 }
