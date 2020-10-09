7942f48136cf4072f7f2dfa2836e2afae10e2316
Lg2K <lg2k-pc@outlook.com>
2020-10-09 13:58:24 +0200

    WS4 - Lever prototype

---
+++ Workshops/2020-10-02, Workshop 4/Assets/Scripts/LeverController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour
+{
+    public LeverManager leverData;
+    public GameObject model;
+
+    private Material modelMaterial;
+    private Transform modelTransform;
+    private bool hasBeenTriggered = false;
+
+    void Start()
+    {
+        modelTransform = model.GetComponent<Transform>();
+        modelMaterial = model.GetComponent<Renderer>().material;
+    }
+
+    void TriggerLever()
+    {
+        hasBeenTriggered = true;
+        leverData.numberOfLeversOn++;
+        modelMaterial.EnableKeyword("_EMISSION");
+        modelTransform.Rotate(new Vector3(180f, 0f, 0f));
+        Debug.Log("Lever triggered");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!hasBeenTriggered && other.CompareTag("Player"))
+        {
+            TriggerLever();
+        }
+    }
+}
